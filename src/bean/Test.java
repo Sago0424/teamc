8622ddbf407fc9db0eb67bf189633d96bc85e528
yantyomsann15 <ktq2390317@stu.o-hara.ac.jp>
2024-04-25 09:57:46 +0900

    Sago

---
+++ src/bean/Test.java
@@ -0,0 +1,50 @@
+package bean;
+
+import java.io.Serializable;
+
+public class Test implements Serializable {
+
+	private Student student;
+	private String classNum;
+	private Subject subject;
+	private School school;
+	private Integer no;
+	private Integer point;
+
+	public Student getStudent(){
+		return student;
+	}
+	public void setStudent(Student student){
+		this.student = student;
+	}
+	public String getClassNum(){
+		return classNum;
+	}
+	public void setClassNum(String classNum){
+		this.classNum = classNum;
+	}
+	public Subject getSubject(){
+		return subject;
+	}
+	public void setSubject(Subject subject){
+		this.subject = subject;
+	}
+	public School getSchool(){
+		return school;
+	}
+	public void setSchool(School school){
+		this.school = school;
+	}
+	public Integer getNo(){
+		return no;
+	}
+	public void setNo(Integer no){
+		this.no = no;
+	}
+	public Integer getPoint(){
+		return point;
+	}
+	public void setPoint(Integer point){
+		this.point = point;
+	}
+}

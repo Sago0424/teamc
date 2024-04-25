732f67ea6539781bd244a346463291dae0cd26b5
user <ktq2390307@stu.o-hara.ac.jp>
2024-04-25 12:00:03 +0900

    あああ

--- src/bean/Test.java
+++ src/bean/Test.java
@@ -4,52 +4,47 @@ import java.io.Serializable;
 
 public class Test implements Serializable {
 
-	private int studentNo; // 学生番号
-	private String classNum; // クラス番号
-	private String subjectCd; // 科目コード
-	private String schoolCd; // 学校コード
-	private int no; // テスト番号
-	private int point; // 点数
+	private Student student;
+	private String classNum;
+	private Subject subject;
+	private School school;
+	private Integer no;
+	private Integer point;
 
-	public int getStudentNo(){
-		return studentNo;
+	public Student getStudent(){
+		return student;
 	}
-	public void setStudentNo(int studentNo){
-		this.studentNo = studentNo;
+	public void setStudent(Student student){
+		this.student = student;
 	}
-
 	public String getClassNum(){
 		return classNum;
 	}
 	public void setClassNum(String classNum){
 		this.classNum = classNum;
 	}
-
-	public String getSubjectCd(){
-		return subjectCd;
+	public Subject getSubject(){
+		return subject;
 	}
-	public void setSubjectCd(String subjectCd){
-		this.subjectCd = subjectCd;
+	public void setSubject(Subject subject){
+		this.subject = subject;
 	}
-
-	public String getSchoolCd(){
-		return schoolCd;
+	public School getSchool(){
+		return school;
 	}
-	public void setSchoolCd(String schoolCd){
-		this.schoolCd = schoolCd;
+	public void setSchool(School school){
+		this.school = school;
 	}
-
-	public int getNo(){
+	public Integer getNo(){
 		return no;
 	}
-	public void setNo(int no){
+	public void setNo(Integer no){
 		this.no = no;
 	}
-
-	public int getPoint(){
+	public Integer getPoint(){
 		return point;
 	}
-	public void setPoint(int point){
+	public void setPoint(Integer point){
 		this.point = point;
 	}
 }
